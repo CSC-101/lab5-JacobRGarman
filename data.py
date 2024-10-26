2661d79c2cc7da12db92c262e1fc40adff71a265
Jacob Garman <jacobgarman06@gmail.com>
2024-10-25 18:16:57 -0700

    Lab 5 Submission

--- data.py
+++ data.py
@@ -1,10 +1,9 @@
 import math
 from typing import Any
 
-
 # Representation of time as hours, minutes, and seconds
 class Time:
-    # Initialize a new Point object.
+    # Initialize a new Time object.
     # input: hour as an int
     # input: minute as an int
     # input: second as an int
@@ -13,18 +12,22 @@ class Time:
         self.minute = minute
         self.second = second
 
-
-    # Provide a developer-friendly string representation of the object.
-    # input: Time for which a string representation is desired. 
-    # output: string representation
-
-
     # Compare the Time object with another value to determine equality.
-    # input: Time against which to compare
     # input: Another value to compare to the Time
     # output: boolean indicating equality
+    def __eq__(self, other: Any) -> bool:
+        # Check that `other` is a Time instance and that its attributes match
+        return (
+            isinstance(other, Time) and
+            self.hour == other.hour and
+            self.minute == other.minute and
+            self.second == other.second
+        )
 
-
+    # Provide a developer-friendly string representation of the object.
+    # output: string representation of the Time instance
+    def __repr__(self) -> str:
+        return f"Time({self.hour}, {self.minute}, {self.second})"
 
 
 # Representation of a two-dimensional point.
@@ -36,20 +39,17 @@ class Point:
         self.x = x
         self.y = y
 
-
     # Provide a developer-friendly string representation of the object.
-    # input: Point for which a string representation is desired. 
     # output: string representation
     def __repr__(self) -> str:
         return 'Point({}, {})'.format(self.x, self.y)
 
-
     # Compare the Point object with another value to determine equality.
-    # input: Point against which to compare
     # input: Another value to compare to the Point
     # output: boolean indicating equality
-    def __eq__(self, other:Any) -> bool:
-        return (other is self or
-                type(other) == Point and
-                math.isclose(self.x, other.x) and
-                math.isclose(self.y, other.y))
+    def __eq__(self, other: Any) -> bool:
+        return (
+            isinstance(other, Point) and
+            math.isclose(self.x, other.x) and
+            math.isclose(self.y, other.y)
+        )
